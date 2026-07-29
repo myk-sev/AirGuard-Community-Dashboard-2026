5b64c70ba1d53fa748c500281215a550bf3f787e
jake-ciliberti <jakeciliberti@gmail.com>
2026-07-29 10:37:11 -0400

    Moved timer into its own file

--- dashboard/static/dashboard/app.js
+++ dashboard/static/dashboard/app.js
@@ -128,26 +128,4 @@
     document.querySelectorAll(".dialog-panel").forEach((panel) => panel.classList.toggle("active", panel.id === `${tab.dataset.tab}-panel`));
     if (tab.dataset.tab === "forecast") await loadForecast();
   }));
-
-  // FIXME: doesn't get called at the right time'
-  document.onLoad()=function(){
-    let time_last_updated = document.querySelector('#last-updated').textContent;
-
-    if (time_last_updated != null) {
-      setTimeout(function() {
-        let prompt_text = ""
-        if (document.documentElement.lang == "en") {
-          prompt_text = "Sensor info may be out of date. Reloading the page is recommended. Click 'OK' to reload.";
-        }
-        else {
-          let prompt_text = "La información del sensor puede estar desactualizada. Se recomienda recargar la página. Haz clic en 'OK' para recargar.";
-        }
-
-        if (confirm(prompt_text)) {
-          location.reload();
-        }
-      }, parseInt(3600 + time_last_updated - Date.now()));
-    }
-  }
-
 })();
